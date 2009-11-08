64f6d4c42e11d8415afe82af277fc6367c57f517
fedepousa <fedepousa@77640596-9b62-11de-bc28-6fc548ead51f>
2009-11-08 07:05:47 +0000

    Subo un poco de la implementacion exacta de max-sat, no anda! le falta poquito pero tengo sueno, luego lo sigo, lo subo por si quieren ir viendo.

---
+++ tp3/Sources/Tp3Exa.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#ifdef TIEMPOS
+#include <sys/time.h>
+#endif
+
+using namespace std;
+
+int resolver(vector< vector<int> > &clausulas, vector<bool> &asignacion);
+bool haceTrue(vector<int> &clausula, vector<bool> &asignacion);
+void siguiente(vector<bool> &asignacion, int cant);
+
+int main(){
+	ifstream entrada("Tp3.in");
+	ofstream salida("Tp3Exa.out");
+	#ifdef TIEMPOS
+	ofstream tiempos("Tp3ExaTiempos.out");
+	timeval inicio;
+	timeval fin;
+	double diferencia;
+	#endif
+	
+	int c; //Cantidad de clausulas
+	int v; //Cantidad de variables
+	int var; // Cantidad de variables dentro de una clausula
+	vector< vector< int > > clausulas; //Aca meto todas las clausulas
+	vector<bool> asignacion; //Asignacion de las variables
+	int res; //Aca voy a acumular el resultado de una asignacion
+	int max=0; //Aca se guarda el maximo total
+	int aux; //Auxiliar para ir levantando
+	
+	while(entrada >> c >> v && !(c==-1 && v==-1)){
+		for(int i=0;i<c;++i){
+			vector< int > clausula;
+			clausulas.push_back(clausula);
+			entrada >> var;
+			for(int j=0;j<var;++j){
+				entrada >> aux;
+				clausulas[0].push_back(aux);
+			}
+		}
+		
+		
+		int cant = 1;
+		for(int i=0;i<v;++i){
+			cant *= 2;
+		} // Con esto cuento la cantidad de asignaciones diferentes que existen
+		
+		for(int i=0;i<v;++i){
+			asignacion.push_back(false);
+		}// Armo la primera asignacion posible poniendo todos en false.
+		
+		
+		for(int i=0;i<cant;++i){
+			res = resolver(clausulas, asignacion);
+			if(res>max){
+				max = res;
+			}
+			siguiente(asignacion,i+1);
+		}
+		
+		salida << max << endl;
+		asignacion.clear();
+		clausulas.clear();
+	}
+	return 0;
+}
+
+int resolver(vector< vector<int> > &clausulas, vector<bool> &asignacion){
+	int res = 0;	
+	int cant = clausulas.size();
+	for(int i=0; i<cant;++i){
+		if(haceTrue(clausulas[i], asignacion)){
+			res++;
+		}
+	}
+	return res;
+}
+
+bool haceTrue(vector<int> &clausula, vector<bool> &asignacion){
+	bool res = false;
+	for(int i=0; i<clausula.size();++i){
+		if(clausula[i]>0){
+			if(asignacion[clausula[i]-1] == true){
+				res = true;
+				break;
+			}	
+		} else {
+			if(asignacion[clausula[i]-1] == false){
+				res = true;
+				break;
+			}
+		}
+	}
+	return res;
+}
+
+void siguiente(vector<bool> &asignacion, int cual){
+	int cant = asignacion.size();
+	for(int i=0;i<cant;++i){
+		if(cual%2 == 0){
+			asignacion[i]=false;
+		} else {asignacion[i]=true;}
+		cual /= 2;
+	}
+}
