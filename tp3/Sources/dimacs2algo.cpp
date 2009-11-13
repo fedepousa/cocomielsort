e2d84b307e31570e35c98c36b927d39a311f208b
castano.rodrigo <castano.rodrigo@77640596-9b62-11de-bc28-6fc548ead51f>
2009-11-13 02:40:21 +0000

    Ahora el conversor junta todas las instancias de los archivos cnf en un mismo archivo del formato del TP.

--- tp3/Sources/dimacs2algo.cpp
+++ tp3/Sources/dimacs2algo.cpp
@@ -1,27 +1,93 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <sys/types.h> 
+#include <dirent.h> 
+#include <string>
+
 
 using namespace std;
 
 void convertir(ifstream &entrada, ofstream &salida);
-int main(int argc, char ** argv) {
-  ofstream salida("conversion.in");
-  string param;
-  if(argc ==1 ) {
-    cerr << "No se suministraron parametros" << endl;
-    cerr << "Uso: " << endl;
-    cerr << argv[0] << " primer_archivo.cnf segundo_archivo.cnf [opcional: mas_archivos.cnf]" << endl;
-    cerr << "El output se hara en el archivo conversion.in, en el formato definido para el TP." << endl; 
-    return 0;
+
+static bool is_target (const string &ext_input, const dirent &, const string &);
+static bool is_file(const dirent &ent);
+static string extension(const string & name);
+static bool contains(const string &c, const string &name) ;
+
+int main(int argc, char * argv[] ) 
+{ 
+   
+    // If a file name contains any of the characters in ignored
+    // such file will not be processed.
+    string ignored = "~";
+    
+    // Files with an extension matching ext_input will be processed.
+    string ext_input = "cnf";
+    
+    // Default value for the name of the output file with the volumes
+    // calculated with Grillado.
+    // Can be configured with command line argument -o "some_file_name"
+    string output_name = "conversion.in";
+    
+    // Default value for the name of the output file with the list
+    // of processed files.
+    // Can be configured with command line 
+    // argument -l "some_list_file_name"
+    string list_name = "list.out";
+    int iteracion = 0;
+    DIR *dir = opendir("."); 
+    ofstream salida(output_name.c_str());
+    ofstream list(list_name.c_str());
+    if(dir) 
+    { 
+        struct dirent *ent; 
+        while((ent = readdir(dir)) != NULL) 
+        { 
+	    	    
+            if(is_target(ext_input,*ent,ignored)) {
+              list << ent->d_name << endl;
+              ifstream entrada(ent->d_name);
+              convertir(entrada, salida);
+            }
+            ++iteracion;
+            //cout << iteracion << endl;
+        } 
+        
+        salida << -1 << " "<< -1  ;
+        cerr << "Se procesan todos todos los archivos con extension cnf." << endl;
+    } 
+    else 
+    { 
+        cout << "Error opening directory." << endl; 
+    } 
+    return 0; 
+}
+static string extension(const string &name) {
+  string res;
+  if(name.rfind(".")!=string::npos) {
+    res = name.substr(name.rfind(".")+1);
   }
-  for(int i = 1 ; i < argc ; ++i) {
-    param = argv[i];
-    ifstream  entrada(param.c_str());
-    convertir(entrada, salida);
+  return res;
+}
+
+static bool is_target (const string &ext_input, const dirent &ent, const string &ignored) {
+  return extension(string(ent.d_name))==ext_input && is_file(ent) && !contains(ignored,string(ent.d_name));
+}
+
+
+static bool contains(const string &c, const string &name) {
+  bool res = false;
+  for(int j=0;j<c.size();++j) {
+    res = res || (name.find(c[j])<name.size());
   }
-  salida << -1 << " "<< -1  ;
-  return 0;
+  return res;
+}
+
+static bool is_file(const dirent & ent) {
+  bool res = ent.d_type == DT_REG;
+  return res;
+  
 }
 
 void convertir(ifstream &entrada, ofstream &salida) {
