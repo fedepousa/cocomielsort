6745d1accab07a291de9485f92a438ffe6b52160
castano.rodrigo <castano.rodrigo@77640596-9b62-11de-bc28-6fc548ead51f>
2009-12-02 06:54:38 +0000

    promedio.cpp ahora te dice el minimo de clausulas satisfechas y el maximo.

--- tp3/Sources/promedio.cpp
+++ tp3/Sources/promedio.cpp
@@ -11,17 +11,23 @@ int main(int argc, char * argv[] ) {
   if(argc>2) {
     ifstream entrada(argv[1]);
     int maximo = atoi(argv[2]);
+    int minimo = maximo;
+    int maxima_instancia = 0;
     int temp;
     int acum = 0;
     int contador = 0;
     while(entrada >> temp) {
       ++contador;
+      minimo = temp < minimo ? temp : minimo;
+      maxima_instancia = temp > maxima_instancia ? temp : maxima_instancia;
       acum += maximo - temp;
     }
     cout.precision(5);
     
     cout << "Archivo .sat: " << argv[1] << endl;
     cout << "Promedio de clausulas insatisfechas: " <<double(acum) / double(contador) << endl;
+    cout << "Minimo de clausulas satisfechas: " <<minimo << endl;
+    cout << "Maximo de clausulas satisfechas: " <<maxima_instancia << endl;
   }
   else {
     cerr << "Uso: " << endl;
