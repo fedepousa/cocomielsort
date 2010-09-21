90a7876ade26daa96df07071223a218b2046090d
gefarion <gefarion@77640596-9b62-11de-bc28-6fc548ead51f>
2010-09-21 20:37:16 +0000

    Agregados los triggers y los SP

---
+++ trunk/Bases/triggers.sql
@@ -0,0 +1,32 @@
+CREATE TRIGGER auditoria_concurso_ins AFTER INSERT ON concurso
+FOR EACH ROW
+INSERT INTO auditoria_concurso(tipo, new_id_camara, new_id_concurso, usuario, fecha)
+VALUES ('Ins',NEW.id_camara, NEW.id, CURRENT_USER(), NOW());
+
+CREATE TRIGGER auditoria_concurso_del AFTER DELETE ON concurso
+FOR EACH ROW
+INSERT INTO auditoria_concurso(tipo, old_id_camara, old_id_concurso, usuario, fecha)
+VALUES ('Del',OLD.id_camara, OLD.id, CURRENT_USER(), NOW());
+
+CREATE TRIGGER auditoria_concurso_upd AFTER UPDATE ON concurso
+FOR EACH ROW
+INSERT INTO auditoria_concurso(tipo, new_id_camara, new_id_concurso,old_id_camara,old_id_concurso, usuario, fecha)
+VALUES ('Upd',NEW.id_camara, NEW.id,OLD.id_camara, OLD.id, CURRENT_USER(), NOW());
+
+
+---------------------------------------------------------------------------------
+
+CREATE TRIGGER auditoria_inscripcion_ins AFTER INSERT ON inscripcion
+FOR EACH ROW
+INSERT INTO auditoria_inscripcion(tipo, new_id, new_orden_merito, new_nombre_universidad, new_promedio, new_fecha_titulo, new_id_concurso, new_cuil_abogado, usuario, fecha)
+VALUES ('Ins',NEW.id, NEW.orden_merito, NEW.nombre_universidad, NEW.promedio, NEW.fecha_titulo, NEW.id_concurso, NEW.cuil_abogado, CURRENT_USER() , NOW());
+
+CREATE TRIGGER auditoria_inscripcion_del AFTER DELETE ON inscripcion
+FOR EACH ROW
+INSERT INTO auditoria_inscripcion(tipo, old_id, old_orden_merito, old_nombre_universidad, old_promedio, old_fecha_titulo, old_id_concurso, old_cuil_abogado, usuario, fecha)
+VALUES ('Del',OLD.id, OLD.orden_merito, OLD.nombre_universidad, OLD.promedio, OLD.fecha_titulo, OLD.id_concurso, OLD.cuil_abogado, CURRENT_USER() , NOW());
+
+CREATE TRIGGER auditoria_inscripcion_upd AFTER UPDATE ON inscripcion
+FOR EACH ROW
+INSERT INTO auditoria_inscripcion(tipo, old_id, old_orden_merito, old_nombre_universidad, old_promedio, old_fecha_titulo, old_id_concurso, old_cuil_abogado,new_id, new_orden_merito, new_nombre_universidad, new_promedio, new_fecha_titulo, new_id_concurso, new_cuil_abogado, usuario, fecha)
+VALUES ('Upd',OLD.id, OLD.orden_merito, OLD.nombre_universidad, OLD.promedio, OLD.fecha_titulo, OLD.id_concurso, OLD.cuil_abogado,NEW.id, NEW.orden_merito, NEW.nombre_universidad, NEW.promedio, NEW.fecha_titulo, NEW.id_concurso, NEW.cuil_abogado, CURRENT_USER() , NOW());
