839df63520ebf1e7adb98bbaca8f73ce5130a41e
castano.rodrigo <castano.rodrigo@77640596-9b62-11de-bc28-6fc548ead51f>
2010-09-24 07:05:20 +0000

    [PLP] Mil cambios, cualquier cosa, si no copa, volvemos a la version anterior, igual no avance casi nada con los ejercicios.

--- PLP/tp.pl
+++ PLP/tp.pl
@@ -5,34 +5,31 @@ hombre(X) :- X = doc.
 % Arranca el TP!!!!!!!
 
 % Ejercicio 1
+% esLista(+X)
 
 esLista([]).
-esLista([X | Xs]) :- esLista(Xs).
+esLista([_ | _]).
 
 
 % Ejercicio 2
-% No usamos literal
 
+% literal(+E, -A)
 literal(Xs, A) :- esLista(Xs), member(A, Xs).
-literal(choice(E, F), A) :- literal(E, A).
-literal(choice(E, F), A) :- literal(F, A).
-literal(concat(E, F), A) :- literal(E, A).
-literal(concat(E, F), A) :- literal(F, A).
+literal(choice(E, _), A) :- literal(E, A).
+literal(choice(_, F), A) :- literal(F, A).
+literal(concat(E, _), A) :- literal(E, A).
+literal(concat(_, F), A) :- literal(F, A).
 literal(rep(E), A) :- literal(E, A).
 
+% Ejemplos
+% literal(choice(choice([a],[a,b]), [a,b,a]),A).
 
 
-literales2([], []).
-literales2([X | Xs], Ls) :- literales2(Xs, CasiLs), append([X], CasiLs, Ls).
-literales2(choice(E, F), Ls) :- literales2(E, CasiLs), literales2(F, CasiLss), append(CasiLs, CasiLss, Ls).
-literales2(concat(E, F), Ls) :- literales2(E, CasiLs), literales2(F, CasiLss), append(CasiLs, CasiLss, Ls).
-literales2(rep(E), Ls) :- literales2(E, Ls).
+% literales(+E, -Sigma)
+literales(E, Sigma) :- setof(X, literal(E,X),Sigma).
 
-literales(E, Sigma) :- literales2(E, CasiSigma), filtrarRepetidos(CasiSigma, Sigma).
-
-filtrarRepetidos([], []).
-filtrarRepetidos([X | Xs], Ls) :- member(X, Xs), filtrarRepetidos(Xs, Ls).
-filtrarRepetidos([X | Xs], Ls) :- not(member(X, Xs)), filtrarRepetidos(Xs, CasiLs), append([X], CasiLs, Ls).
+% Ejemplos
+% literales(choice(choice([a],[a,b]), [a,b,a]),Sigma).
 
 
 % Ejercicio 3
@@ -40,54 +37,87 @@ filtrarRepetidos([X | Xs], Ls) :- not(member(X, Xs)), filtrarRepetidos(Xs, CasiL
 
 vacia([]).
 
+% match(+E, +Palabra)
 match(Xs, P) :- esLista(Xs), P = Xs.
-match(choice(E, F), P) :- match(E, P).
+match(choice(E, _), P) :- match(E, P).
 match(choice(E, F), P) :- not(match(E, P)), match(F, P).
-match(concat(E, F), P) :- match(E, P, M, T), match(F, T).
-match(rep(E), P) :- match(rep(E), P, CasiP, Tail), vacia(Tail).  
+% match(concat(E, F), P) :- match(E, P, _, T), match(F, T).
+% la de arriba es la primera implementacion, la reemplazamos por la de abajo.
+match(concat(E, F), P) :- append(A,B, P), match(A, E), match(B, F).
+match(rep(E), P) :- match(rep(E), P, _, Tail), vacia(Tail).  
 
 
+% match(+E, +S, -Matched, -Tail)
 match(Xs, S, Xs, T) :- esLista(Xs), append(Xs, T, S).
-match(choice(E, F), S, M, T) :- match(E, S, M, T). 
-match(choice(E, F), S, M, T) :- match(F, S, M, T). 
+match(choice(E, _), S, M, T) :- match(E, S, M, T). 
+match(choice(_, F), S, M, T) :- match(F, S, M, T). 
 match(concat(E, F), S, M, T) :- match(E, S, CasiM, Tail), match(F, Tail, CasiMDos, T), append(CasiM, CasiMDos, M). 
 match(rep(E), S, S, []) :- match(E,S).
 match(rep(E), S, M, T) :- not(match(E,S)), match(E, S, CasiM, Tail), match(rep(E), Tail, CasiMDos, T), append(CasiM, CasiMDos, M). 
 
+% Ejemplos
 % match(rep(concat([a], choice([a], [b]))), [a, b]).
 
 
 % Ejercicio 4
 
+% instanciarConPre(+Xs, +Ys, ?Y) es verdadero cando Y es una lista con
+% alguno de los elementos de Xs seguido de Ys.
+instanciarConPre([X | _] , Y, [ X | Y ]) :- esLista(Y).
+instanciarConPre([_ | Xs] , Y, P) :- esLista(Y), instanciarConPre(Xs, Y, P).
 
-agregarPre(X, [],[]).
-agregarPre(X, [Y], [[X|Y]]) :- esLista(Y).
-agregarPre(X, [Y|Ys], Ls) :- not(vacia(Ys)), agregarPre(X, Ys, CasiLs), append([X], Y, Aux), append([Aux], CasiLs, Ls).
 
-agregarPreLista([], P, P).
-agregarPreLista([X], Ls, P) :- agregarPre(X, Ls, P).
-agregarPreLista([X | [Y | Xs]], Ls, P) :- agregarPreLista([Y | Xs], Ls, CasiP), agregarPre(X, Ls, OtroP), append(CasiP, OtroP, P). 
+% palabrasExacto(+K, +Sigma, -P)
 
-palabrasExacto(0, Sigma, []).
-palabrasExacto(1, [X | Xs], P) :- palabrasExacto(1, Xs, CasiP), append([[X]], CasiP, P).
+palabrasExacto(0, _, []).
 palabrasExacto(K, [], []) :- K \= 0. 
-palabrasExacto(K, [X | Xs], P) :- K > 1, AntK is K-1, palabrasExacto(AntK, [X | Xs], CasiP), agregarPreLista([X | Xs], CasiP, P). 
+palabrasExacto(K, [X | Xs], P) :- K > 0, AntK is K-1, palabrasExacto(AntK, [X | Xs], CasiP), instanciarConPre([X | Xs], CasiP, P). 
 
-palabrasHasta(1, Sigma, P) :- palabrasExacto(1, Sigma, P).
-palabrasHasta(K, Sigma, P) :- K > 1, palabrasExacto(K, Sigma, CasiP), AntK is K-1, palabrasHasta(AntK, Sigma, AuxP), append(CasiP, AuxP, P).
+% palabrasHasta(+K, +Sigma, -P)
+palabrasHasta(0, Sigma, P) :- palabrasExacto(0, Sigma, P).
+palabrasHasta(K, Sigma, P) :- K > 0, palabrasExacto(K, Sigma, P).
+palabrasHasta(K, Sigma, P) :- K > 0, AntK is K-1, palabrasHasta(AntK, Sigma, P).
 
 
 % Ejercicio 5
 
-lenguajeSimpleHasta(K, E, P) :- literales(E, Sigma), palabrasHasta(K, Sigma, P), match(E, P).
-
+% lenguajeSimpleHasta(+K, +E, -P)
 % Hay que preguntar si el 4 tiene que devolver una lista con todas las palabras o si tiene que ir listando de a una, porque asi queda
 % consistente con lo que pide en el 5 para hacer generate and test
 
+lenguajeSimpleHasta(K, E, P) :- literales(E, Sigma), palabrasHasta(K, Sigma, P), match(E, P).
+
+% Ejemplos
+% lenguajeSimpleHasta(3,choice(choice([a],[a,b]), [a,b,a]),P).
+% literales(choice(choice([a],[a,b]), [a,b,a]),Sigma).
 
 
 
 
 
+% Ejercicio 6
+
+% longitud(?K, +E) dada una lista E, longitud(K, E) es verdadero 
+% cuando K es la longitud de la lista.
+longitud(0, []).
+longitud(K, [_ | Xs]) :- longitud(AntK, Xs), K is AntK+1.
 
 
+% matchExacto(+K, +E,-P) enumero todas las palabras de longitud K
+% que pertenecen a L(E), aprovechando la estructura de E.
+
+matchExacto(K, E, P ) :- esLista(E), longitud(K, E), P = E.
+matchExacto(K, choice(E,_), P ) :- matchExacto(K, E, P).
+matchExacto(K, choice(_,F), P ) :- matchExacto(K, F, P).
+matchExacto(K, concat(E,F), P ) :-  between(1, K, Ke),
+                                    matchExacto(Ke, E, PrimerP),
+                                    Kf is K - Ke,
+                                    matchExacto(Kf, F, SegundoP),
+                                    append(PrimerP,SegundoP,P).
+matchExacto(K, rep(E), P ) :-   between(1, K, Ke),
+                                matchExacto(Ke, E, PrimerP),
+                                Kf is K - Ke,
+                                matchExacto(Kf, rep(E), SegundoP),
+                                append(PrimerP, SegundoP, P).
+matchExacto(K, rep(E), P ) :-   matchExacto(K, E, P).
+% matchExacto(3,concat(choice([a,a],[b]),choice(choice([a],[a,b]), [a,b,a])),P).
