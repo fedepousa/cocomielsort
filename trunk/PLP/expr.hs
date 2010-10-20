77fc2c6c0ce2f29eaee42a2d68c9cb6ad9582121
fedepousa <fedepousa@77640596-9b62-11de-bc28-6fc548ead51f>
2010-10-20 15:21:02 +0000

    bla

--- trunk/PLP/expr.hs
+++ trunk/PLP/expr.hs
@@ -1,5 +1,28 @@
 module Expr where
+import Equiv
 import List
 
 -- Definicion del tipo de datos
 
+data Exp = Cte Integer | Suma Exp Exp | Mult Exp Exp | Var String | Let String Exp Exp deriving (Eq, Show)
+
+type Contexto = [(String, Integer)]
+
+
+-- Ejercicio 9
+--TODO
+
+-- Ejercicio 10
+--TODO
+
+-- Ejercicio 11
+--TODO
+
+-- Ejercicio 12
+--TODO
+
+-- Ejercicio 13
+--TODO
+
+-- Ejercicio 14
+--TODO
