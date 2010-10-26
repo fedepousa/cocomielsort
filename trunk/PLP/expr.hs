b107efed62d33f2ed225742c3dc54625eea8aed8
facu.zeta <facu.zeta@77640596-9b62-11de-bc28-6fc548ead51f>
2010-10-26 17:26:12 +0000

    - 9   Me parece que el fold esta mal hecha, la parte del Let, pero la deja asi para que sea facil evaluar  - 11  Me parece que estoy haciendo  recurcion explitia aunqeu no esta tan explicita, supongo que en el momento en que fold se cambie para bien, hay que arreglar esto  - 12 Idem -  13 Version correcta pero no entregable, puede servir para cotejar que funcione la proxima -   14 requiere modulo equiv

--- trunk/PLP/expr.hs
+++ trunk/PLP/expr.hs
@@ -1,5 +1,5 @@
 module Expr where
-import Equiv
+-- import Equiv
 import List
 
 -- Definicion del tipo de datos
@@ -10,19 +10,56 @@ type Contexto = [(String, Integer)]
 
 
 -- Ejercicio 9
---TODO
+fold::(Integer->b)->(b->b->b)->(b->b->b)->(String->b)->(String->Exp->Exp->b)->Exp->b
+fold f_cte f_suma f_mult f_var f_let (Cte int) = f_cte int
+fold f_cte f_suma f_mult f_var f_let (Suma exp1 exp2) = f_suma (fold f_cte f_suma f_mult f_var f_let exp1) (fold f_cte f_suma f_mult f_var f_let exp2)
+fold f_cte f_suma f_mult f_var f_let (Mult exp1 exp2) = f_mult (fold f_cte f_suma f_mult f_var f_let exp1) (fold f_cte f_suma f_mult f_var f_let exp2)
+fold f_cte f_suma f_mult f_var f_let (Var str) = f_var str
+fold f_cte f_suma f_mult f_var f_let (Let str exp1 exp2) = f_let str exp1 exp2  -- CREO Q LA ESTOY CAGANDO, NO ESTOY SEGURO
 
 -- Ejercicio 10
---TODO
+agregar::Contexto->String->Integer->Contexto
+agregar c n v = (n,v) : filter (\x-> (fst x /= n ) )  c
 
 -- Ejercicio 11
---TODO
+evaluar::Exp->Integer
+evaluar exp = evaluarPrima exp []
+
+evaluarPrima::Exp->Contexto->Integer
+evaluarPrima exp contexto = fold f_cte f_suma f_mult f_var f_let exp
+				where 
+					f_cte = id
+					f_suma = (+)
+					f_mult = (*)
+					f_var = \str			-> snd (head ( filter (\x-> ( fst x == str)) contexto))
+					f_let = \str exp1 exp2  -> evaluarPrima exp2 ( agregar contexto str (evaluarPrima exp1 contexto) )   --WARNING: ESTO ES RECURCION EXPLICITA?
 
 -- Ejercicio 12
---TODO
-
+evaluar_prudentePrima:: Exp -> Contexto -> Maybe Integer
+evaluar_prudentePrima exp contexto = fold f_cteP f_sumaP f_multP f_varP f_letP exp
+	where 
+	f_varP str =  if  (length ( ( filter (\x-> ( fst x == str)) contexto)) >0) then Just (snd (head ( filter (\x-> ( fst x == str)) contexto))) 
+				   else Nothing
+	f_letP = \str exp1 exp2 -> (if ( (evaluar_prudentePrima exp1 contexto)==Nothing) then Nothing 	 --WARNING: ESTO ES RECURCION EXPLICITA?
+								else evaluar_prudentePrima exp2 ( agregar contexto str (evaluarPrima exp1 contexto))) 
+	f_cteP x= Just x
+	f_sumaP (Just x) (Just y) = Just (x + y)
+	f_sumaP Nothing _ = Nothing
+	f_sumaP _ _ = Nothing
+	f_multP (Just x) (Just y) = Just (x * y)
+	f_multP Nothing _ = Nothing
+	f_multP _ _ = Nothing
+	
+						
 -- Ejercicio 13
---TODO
+
+--TODO: VERSION CORRECTA PERO NO ENTREGABLE PORQUE TIENE RECURSION EXPLICITA
+generar::Integer->Integer->[Exp]
+generar k h =  [ (Cte n) | n<-[0..k] ]  ++ nub (generarPrima k h)
+
+generarPrima::Integer->Integer->[Exp]
+generarPrima k 1 =  [ (Cte n) | n<-[0..k] ] 
+generarPrima k h =  nub (concat[  [(Suma izq der) , (Mult izq der)] |  izqH<-[1..h-1] , derH <-[1..h-1]  , izq<-(generar k izqH) , der<-(generar k derH)] )
 
 -- Ejercicio 14
 --TODO
