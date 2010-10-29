7c276e83558a198939d652dc5f4c5c76824e127f
facu.zeta <facu.zeta@77640596-9b62-11de-bc28-6fc548ead51f>
2010-10-29 02:36:48 +0000

    Todo codigo completo Faltan comentarios!!

--- trunk/PLP/expr.hs
+++ trunk/PLP/expr.hs
@@ -1,30 +1,42 @@
 module Expr where
--- import Equiv
+import Equiv
 import List
+ 
+
 
 -- Definicion del tipo de datos
 
 data Exp = Cte Integer | Suma Exp Exp | Mult Exp Exp | Var String | Let String Exp Exp deriving (Eq, Show)
 
-type Contexto = [(String, Integer)] 
+type Contexto = [(String, Integer)]
 
-
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Ejercicio 9
+{-
+El fold correspondiente al tipo.
+Respetando el esquema clasico para datos de varios constructores
+Cuando usamos evaluar b tipa (Contexto -> Integer )
+cuando usamos evaluar_prudente b tipa (Contexto -> Maybe Integer )
+-}
 fold::(Integer->b)->(b->b->b)->(b->b->b)->(String->b)->(String->b->b->b)->Exp->b
--- Cuando usamos evaluar b tipa (Contexto -> Integer )
--- cuando usamos evaluar_prudente b tipa (Contexto -> Maybe Integer )
 fold f_cte f_suma f_mult f_var f_let (Cte int) = f_cte int
 fold f_cte f_suma f_mult f_var f_let (Suma exp1 exp2) = f_suma (fold f_cte f_suma f_mult f_var f_let exp1) (fold f_cte f_suma f_mult f_var f_let exp2)
 fold f_cte f_suma f_mult f_var f_let (Mult exp1 exp2) = f_mult (fold f_cte f_suma f_mult f_var f_let exp1) (fold f_cte f_suma f_mult f_var f_let exp2)
 fold f_cte f_suma f_mult f_var f_let (Var str) = f_var str
 fold f_cte f_suma f_mult f_var f_let (Let str exp1 exp2) = f_let str (fold f_cte f_suma f_mult f_var f_let exp1) (fold f_cte f_suma f_mult f_var f_let exp2)
 
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Ejercicio 10
+--Agrega al contexto el par correspondiente
 agregar::Contexto->String->Integer->Contexto
 agregar c n v = (n,v) : filter (\x-> (fst x /= n ) )  c
 
-
-
+ 
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Ejercicio 11
 {-La idea del cambio en este ejercicio es que el fold me devuelva en vez de valores funciones
 las funciones que me devuelven son del tipo Contexto -> a, hago esto para resolver facil el caso del let
@@ -40,50 +52,63 @@ evaluar exp = evaluarPrima exp []
 
 evaluarPrima::Exp->Contexto->Integer
 evaluarPrima = fold f_cte f_suma f_mult f_var f_let
-				where 
-					f_cte = \x contexto -> x
-					f_suma = \x y contexto -> (x contexto) + (y contexto)
-					f_mult = \x y contexto -> (x contexto) * (y contexto)
-					f_var = \str contexto		-> snd (head ( filter (\x-> ( fst x == str)) contexto))
-					f_let = \str exp1F exp2F contexto -> exp2F (agregar contexto  str (exp1F contexto))
-				
+    where 
+     f_cte = \x contexto -> x
+     f_suma = \x y contexto -> (x contexto) + (y contexto)
+     f_mult = \x y contexto -> (x contexto) * (y contexto)
+     f_var = \str contexto  -> snd (head ( filter (\x-> ( fst x == str)) contexto))
+     f_let = \str exp1F exp2F contexto -> exp2F (agregar contexto  str (exp1F contexto))
 
+   
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Ejercicio 12
 evaluar_prudente::Exp->Maybe Integer
 evaluar_prudente exp = evaluar_prudentePrima exp []
 
 evaluar_prudentePrima::Exp->Contexto->Maybe Integer
 evaluar_prudentePrima = fold f_cteP f_sumaP f_multP f_varP f_letP 
-				where 
-					f_cteP = \x contexto -> Just x
-					f_sumaP = \x y contexto -> if (algunoEsNothing (x contexto) (y contexto) ) then Nothing 
-											   else  ( mybeFunc (+) (x contexto) (y contexto)  )
-					f_multP = \x y contexto -> if (algunoEsNothing (x contexto) (y contexto) ) then Nothing 
-											   else  ( mybeFunc (*) (x contexto) (y contexto)  )
-					f_varP = \str contexto ->  if  (length ( ( filter (\x -> ( fst x == str)) contexto)) ==0) then Nothing
-											   else Just (snd (head ( filter (\x-> ( fst x == str)) contexto)))
-					f_letP = \str exp1F exp2F contexto -> if (algunoEsNothing (exp1F contexto) (exp1F contexto)) then Nothing
-														  else exp2F (agregar contexto  str (sinNothing (exp1F contexto)))
+    where 
+     f_cteP = \x contexto -> Just x
+     f_sumaP = \x y contexto -> if (algunoEsNothing (x contexto) (y contexto) ) then Nothing 
+              else  ( mybeFunc (+) (x contexto) (y contexto)  )
+     f_multP = \x y contexto -> if (algunoEsNothing (x contexto) (y contexto) ) then Nothing 
+              else  ( mybeFunc (*) (x contexto) (y contexto)  )
+     f_varP = \str contexto ->  if  (length ( ( filter (\x -> ( fst x == str)) contexto)) ==0) then Nothing
+              else Just (snd (head ( filter (\x-> ( fst x == str)) contexto)))
+     f_letP = \str exp1F exp2F contexto -> if (algunoEsNothing (exp1F contexto) (exp1F contexto)) then Nothing
+                else exp2F (agregar contexto  str (sinNothing (exp1F contexto)))
 
 algunoEsNothing Nothing _ = True
 algunoEsNothing _ Nothing = True
 algunoEsNothing _ _ = False
 
 mybeFunc f (Just x) (Just y) = Just (f x y)
+sinNothing (Just x) = x
 
-sinNothing (Just x) = x 
 
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Ejercicio 13
-
---TODO: VERSION CORRECTA PERO NO ENTREGABLE PORQUE TIENE RECURSION EXPLICITA
+{-
+La idea fue usar foldr
+Lista inicial tenemos [1..h-1]
+Caso base tenemos la lista con las ctes definidas ( de cero a k)
+Y la funcion lo que hace es tomar un elemento y la lista (ya foldeada), itera en los elementos de esa lista, y devuelve la lista original mas lo siguiente:
+Toma cada elemento de la lista y lo convina con sus otros elementos usando los dos constructores binarios y asi construye los del siguiente nivel.
+Esto lo va a hacer h veces dado que la lista inicial tiene esa cantidad de elementos
+ -}
 generar::Integer->Integer->[Exp]
-generar k h =  [ (Cte n) | n<-[0..k] ]  ++ nub (generarPrima k h)
-
-generarPrima::Integer->Integer->[Exp]
-generarPrima k 1 =  [ (Cte n) | n<-[0..k] ] 
-generarPrima k h =  nub (concat[  [(Suma izq der) , (Mult izq der)] |  izqH<-[1..h-1] , derH <-[1..h-1]  , izq<-(generar k izqH) , der<-(generar k derH)] )
-
+generar k h = foldr (\x lista -> concat[[Suma (lista!!e1) (lista!!e2), Mult (lista!!e1) (lista!!e2) ] | e1<-[0..(length lista -1)], e2<-[e1..(length lista -1)]   ] ++ lista ) [Cte i | i<-[0..k]] [1..h-1]
+ 
+ 
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Ejercicio 14
---TODO
+sinJust (Just x) = x
+isNothing Nothing = True
+isNothing _ = False
 
-
+generar_clases::Integer->Integer->Ce Exp
+generar_clases k h = ( C (generar k h) (\x ->if (isNothing (evaluar_prudente x)) then [] 
+        else ([e | e<- (generar k h) ,   (evaluar_prudente x) ==  (evaluar_prudente e)]  ) ))
