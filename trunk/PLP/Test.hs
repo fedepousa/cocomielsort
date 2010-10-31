cb889b914f574e9321ca8a5ef8317bd4a621c283
fedepousa <fedepousa@77640596-9b62-11de-bc28-6fc548ead51f>
2010-10-31 23:09:57 +0000

    Estaria todo menos los tests de fold

--- trunk/PLP/Test.hs
+++ trunk/PLP/Test.hs
@@ -15,6 +15,11 @@ test = do runTestTT tests1
           runTestTT tests6
           runTestTT tests7
           runTestTT tests8
+          runTestTT tests10
+          runTestTT tests11
+          runTestTT tests12
+          runTestTT tests13
+          runTestTT tests14
 
 
 
@@ -101,17 +106,45 @@ tests8 = TestList [TestLabel "Test30" test30, TestLabel "Test31" test31]
 
 --Ejercicio 10
 
+test33 = TestCase (assertEqual "Ejercicio 10 test 1" (agregar [] "A" 3) [("A", 3)])
+test34 = TestCase (assertEqual "Ejercicio 10 test 2" (agregar [("B", 4)] "A" 3) [("A", 3), ("B",4)])
+test35 = TestCase (assertEqual "Ejercicio 10 test 3" (agregar [("A", 7), ("B", 4), ("A", 8)] "A" 3) [("A", 3), ("B",4)])
+
+tests10 = TestList [TestLabel "Test33" test33, TestLabel "Test34" test34, TestLabel "Test35" test35]
 
 --Ejercicio 11
+test36 = TestCase (assertEqual "Ejercicio 11 test 1" (evaluar (Suma (Cte 1) (Cte 2))) 3)
+test37 = TestCase (assertEqual "Ejercicio 11 test 2" (evaluar (Mult (Suma (Cte 3) (Cte 4)) (Cte 2))) 14)
+test38 = TestCase (assertEqual "Ejercicio 11 test 3" (evaluar (Mult (Suma (Cte 3) (Cte 4)) (Let "X" (Cte 2) (Var "X")))) 14)
 
+tests11 = TestList [TestLabel "Test36" test36, TestLabel "Test37" test37, TestLabel "Test38" test38]
 
 --Ejercicio 12
 
+test39 = TestCase (assertEqual "Ejercicio 12 test 1" (evaluar_prudente (Suma (Cte 1) (Cte 2))) (Just 3))
+test40 = TestCase (assertEqual "Ejercicio 12 test 2" (evaluar_prudente (Mult (Suma (Cte 3) (Cte 4)) (Let "X" (Cte 2) (Var "X")))) (Just 14))
+test41 = TestCase (assertEqual "Ejercicio 12 test 3" (evaluar_prudente (Var "X")) Nothing)
+test42 = TestCase (assertEqual "Ejercicio 12 test 4" (evaluar_prudente (Mult (Suma (Cte 3) (Var "Y")) (Let "X" (Cte 2) (Var "X")))) Nothing)
+
+tests12 = TestList [TestLabel "Test39" test39, TestLabel "Test40" test40, TestLabel "Test41" test41, TestLabel "Test42" test42]
 
 --Ejercicio 13
 
+test43 = TestCase (assertEqual "Ejercicio 13 test 1" (generar 0 1) [Cte 0])
+test44 = TestCase (assertEqual "Ejercicio 13 test 2" (generar 0 2) [Suma (Cte 0) (Cte 0), Mult (Cte 0) (Cte 0), (Cte 0)])
+test45 = TestCase (assertEqual "Ejercicio 13 test 3" (length (generar 0 3)) 19)
+test46 = TestCase (assertEqual "Ejercicio 13 test 4" (generar 4 1) [Cte 0, Cte 1, Cte 2, Cte 3, Cte 4])
+test47 = TestCase (assertEqual "Ejercicio 13 test 5" (length (generar 4 2)) 55)
+
+tests13 = TestList [TestLabel "Test43" test43, TestLabel "Test44" test44, TestLabel "Test45" test45, TestLabel "Test46" test46, TestLabel "Test47" test47]
 
 --Ejercicio 14
+
+test48 = TestCase (assertEqual "Ejercicio 14 test 1" (dominio (generar_clases 0 3)) (generar 0 3))
+test49 = TestCase (assertEqual "Ejercicio 14 test 2" (clase (generar_clases 1 2) (Suma (Cte 0) (Cte 1))) [Suma (Cte 0) (Cte 1), Suma (Cte 1) (Cte 0), Mult (Cte 1) (Cte 1), Cte 1])
+test50 = TestCase (assertEqual "Ejercicio 14 test 3" (clase (generar_clases 1 2) (Suma (Cte 4) (Cte 1))) [])
+
+tests14 = TestList [TestLabel "Test48" test48, TestLabel "Test49" test49, TestLabel "Test50" test50] 
 
 
 
