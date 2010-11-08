855d528300d51bb308c8845c860ab9b884df7312
gefarion <gefarion@77640596-9b62-11de-bc28-6fc548ead51f>
2010-11-08 03:04:23 +0000

    Agregada SwapLeavesHeuristic y varias funciones auxiliares en TreeHelper

--- trunk/Bases/Tp2/UBADBTools_2010_2C/src/ubadbtools/queryOptimizer/common/TreeHelper.java
+++ trunk/Bases/Tp2/UBADBTools_2010_2C/src/ubadbtools/queryOptimizer/common/TreeHelper.java
@@ -1,6 +1,111 @@
 package ubadbtools.queryOptimizer.common;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ubadbtools.queryOptimizer.common.conditions.FieldOperand;
+import ubadbtools.queryOptimizer.common.conditions.QueryCondition;
+import ubadbtools.queryOptimizer.common.conditions.QuerySingleCondition;
+import ubadbtools.queryOptimizer.common.relation.RelationNode;
+//import ubadbtools.queryOptimizer.common.selection.SelectionNode;
+//import ubadbtools.queryOptimizer.common.product.ProductNode;
+//import ubadbtools.queryOptimizer.common.projection.ProjectionNode;
+import ubadbtools.queryOptimizer.common.selection.SelectionNode;
+
 public class TreeHelper
 {
-	//Completar con operaciones sobre el árbol comunes a más de una heurística o que consideren útil que venga acá
+	
+	public List<String> aliases(QueryNode qN)
+	{
+		List<String> res = new ArrayList<String>();
+		List<RelationNode> aux = tablas(qN);
+		for(RelationNode actual : aux)
+			res.add(actual.getRelationAlias());
+		return res;
+		
+	}
+	
+	//Esta funcion devuelve una lista con todas las condiciones de junta
+	public List<QuerySingleCondition> condicionesJunta(QueryNode qN){
+		List<QuerySingleCondition> aux;
+		List<QuerySingleCondition> res = new ArrayList<QuerySingleCondition>();
+		
+		aux = condiciones(qN);
+		for (QuerySingleCondition actual : aux){
+			if (actual.isJoinCondition())
+			{
+				res.add(actual);
+			}
+		}
+		return res;
+	}
+	
+	//Esta funcion arma una lista con todas las condiciones
+	public List<QuerySingleCondition> condiciones(QueryNode qN){
+		QueryCondition caux;
+		QuerySingleCondition caux2;
+		List<QuerySingleCondition> aux = new ArrayList<QuerySingleCondition>();
+		
+		if (qN.isProjection()){
+			return condiciones(((QuerySingleInputNode) qN).getLowerNode());
+		}
+		
+		if (qN.isSelection()){
+			caux = ((SelectionNode) qN).getCondition();
+			caux2 = (QuerySingleCondition) caux;
+			aux.add(caux2);
+			aux.addAll(condiciones(((QuerySingleInputNode) qN).getLowerNode()));
+			return aux;
+		}
+		
+		if (qN.isProduct()){
+			aux.addAll(condiciones(((QueryDoubleInputNode) qN).getLeftLowerNode()));
+			aux.addAll(condiciones(((QueryDoubleInputNode) qN).getRightLowerNode()));
+			return aux;
+		}
+		
+		if (qN.isRelation()){
+			return aux;
+		}
+		
+		return aux;
+	}
+	
+	
+	//Esta funcion dado un alias me devuelve el nodo correspondiente a la tabla de ese alias
+	public RelationNode alias2tabla(QueryNode qN, String alias)
+	{
+		List<RelationNode> aux;
+		aux = tablas(qN);
+		for(RelationNode actual: aux){
+			if (actual.getRelationAlias()==alias) 
+				return actual;
+		}
+		return null;
+	}
+	
+	//Esta funcion retorna una lista con los Nodos de las Relaciones
+	public List<RelationNode> tablas(QueryNode qN)
+	{
+		List<RelationNode> aux = new ArrayList<RelationNode>();
+		if (qN.isProjection()){
+			return tablas(((QuerySingleInputNode) qN).getLowerNode());
+		}
+		
+		if (qN.isSelection()){
+			return tablas(((QuerySingleInputNode) qN).getLowerNode());
+		}
+		
+		if (qN.isProduct()){
+			aux.addAll(tablas(((QueryDoubleInputNode) qN).getLeftLowerNode()));
+			aux.add((RelationNode) ((QueryDoubleInputNode) qN).getRightLowerNode());
+			return aux;
+		}
+		
+		if (qN.isRelation()){
+			aux.add((RelationNode) qN);
+			return aux;
+		}
+		return aux;
+	}
 }
--- trunk/Bases/Tp2/UBADBTools_2010_2C/src/ubadbtools/queryOptimizer/optimizer/heuristics/SwapLeavesHeuristic.java
+++ trunk/Bases/Tp2/UBADBTools_2010_2C/src/ubadbtools/queryOptimizer/optimizer/heuristics/SwapLeavesHeuristic.java
@@ -1,6 +1,20 @@
 package ubadbtools.queryOptimizer.optimizer.heuristics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import ubadbtools.queryOptimizer.common.QueryNode;
+import ubadbtools.queryOptimizer.common.QuerySingleInputNode;
+import ubadbtools.queryOptimizer.common.conditions.FieldOperand;
+import ubadbtools.queryOptimizer.common.conditions.QueryAndSingleConditions;
+import ubadbtools.queryOptimizer.common.conditions.QueryCondition;
+import ubadbtools.queryOptimizer.common.conditions.QuerySingleCondition;
+import ubadbtools.queryOptimizer.common.product.ProductNode;
+import ubadbtools.queryOptimizer.common.projection.ProjectionNode;
+import ubadbtools.queryOptimizer.common.relation.RelationNode;
+import ubadbtools.queryOptimizer.common.selection.SelectionNode;
+import ubadbtools.queryOptimizer.common.TreeHelper;
+import ubadbtools.queryOptimizer.gui.QueryNodeGuiMapper;
 
 /**
  * 
@@ -16,8 +30,108 @@ public class SwapLeavesHeuristic extends Heuristic
 	}
 	
 	@Override
-	protected void internalApplyHeuristic(QueryNode queryNode)
+	protected void internalApplyHeuristic(QueryNode qN)
 	{
+		TreeHelper th = new TreeHelper();
+		
+		boolean salir;
+		RelationNode t1,t2;
+		String s1,s2;
+		ProductNode nuevaBase, pAux;
+		nuevaBase =null;
+		
+		//Calculo las tablas con las relaciones y las cond de junta
+		List<QuerySingleCondition> condsJunta = th.condicionesJunta(qN);
+		
+		//En esta lista guardo las tablas que ya fueron procedadas (solo el alias)
+		List<String> procesadas = new ArrayList<String>();
+		
+		//Por procesar
+		List<RelationNode> tablasFaltantes = th.tablas(qN);
+		
+		if (!condsJunta.isEmpty()){
+			//Creo el primer producto utilizando la primer condicion de junta
+			nuevaBase = new ProductNode();
+			s1=((FieldOperand) (condsJunta.get(0).getLeftOperand())).getField().getRelationAlias();
+			t1= th.alias2tabla(qN, s1);
+			s2=((FieldOperand) (condsJunta.get(0).getRightOperand())).getField().getRelationAlias();
+			t2= th.alias2tabla(qN, s2);
+			nuevaBase.linkWith(t1, t2);
+			condsJunta.remove(0);
+			tablasFaltantes.remove(t1);
+			tablasFaltantes.remove(t2);
+			procesadas.add(s1);
+			procesadas.add(s2);
+		}
+		
+		while(!condsJunta.isEmpty()){
+			//Uso este flag para indicar si agregue un nodo o no
+			salir=false;
+			//Busco entre las condiciones restantes alguna que pueda unir con lo que ya calcule
+			for(QuerySingleCondition actual : condsJunta){
+				s1=((FieldOperand) (actual.getLeftOperand())).getField().getRelationAlias();
+				s2=((FieldOperand) (actual.getRightOperand())).getField().getRelationAlias();
+				if (procesadas.contains(s1)){
+					pAux = new ProductNode();
+					t2= th.alias2tabla(qN, s2);
+					pAux.linkWith(nuevaBase,t2);
+					nuevaBase = pAux;
+					procesadas.add(s2);
+					tablasFaltantes.remove(t2);
+					condsJunta.remove(actual);
+					salir=true;
+				}
+				
+				if (salir) break;
+				
+				if (procesadas.contains(s2)){
+					pAux = new ProductNode();
+					t1= th.alias2tabla(qN, s1);
+					pAux.linkWith(nuevaBase,t1);
+					nuevaBase = pAux;
+					procesadas.add(s1);
+					tablasFaltantes.remove(t1);
+					condsJunta.remove(actual);
+					salir=true;
+				}
+				
+				if (salir) break;
+			}
+			if (!salir){
+				pAux = new ProductNode();
+				s1=((FieldOperand) (condsJunta.get(0).getLeftOperand())).getField().getRelationAlias();
+				t1= th.alias2tabla(qN, s1);
+				pAux.linkWith(nuevaBase,t1);
+				nuevaBase = pAux;
+				procesadas.add(s1);
+			}
+		}
+		
+		//Proceso las tablas que no tenian ninguna condicion de junta
+		if (!tablasFaltantes.isEmpty())
+			for (RelationNode actual : tablasFaltantes){
+				pAux = new ProductNode();
+				pAux.linkWith(nuevaBase,actual);
+				nuevaBase = pAux;
+			}
+		
+		//Busco la base del arbol y la modifico en caso de ser necesario
+		if (qN.isRelation() || qN.isProduct()) return;
+		
+		if (qN.isProjection()){
+			qN = ((ProjectionNode) qN).getLowerNode();
+		}
+		
+		if (!qN.isSelection()) return;
+		
+		while (qN.isSelection()){
+			//Vay al final de las selecciones
+			qN = ((QuerySingleInputNode) qN).getLowerNode();
+		}
+		
+		qN = qN.getUpperNode();
+		((QuerySingleInputNode) qN).linkWith(nuevaBase);
+				
 		System.out.println("SwapLeavesHeuristic");
 	}
 }
